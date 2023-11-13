8374c467511c080522ebd8f9130e896aa28b2016
D'Andre Lambert <dandrel@usca.edu>
2023-11-13 12:57:55 -0500

    new changes

--- js/changeLink.js
+++ js/changeLink.js
@@ -8,3 +8,26 @@
 
     });
 // console.log("I am here");
+
+// play with jQuery .each function
+
+// update the img link
+$('img').each(function(){
+    var path = $(this).attr('src');
+    var newpath = "img/" + path;
+    $(this).attr("src", newpath);
+    // console.log(newpath);
+
+});
+
+// update the anchor link
+
+$('a').each(function(){
+    var path = $(this).attr('href');
+    var newpath = "Google" + ".com";
+    $(this).attr("href", newpath);
+    console.log(newpath)
+});
+
+// Text manipulation
+$('a').each(function(){});
