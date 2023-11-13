4ec78666719ef9b9c191361f657c8f7d5098e8cc
D'Andre Lambert <dandrel@usca.edu>
2023-11-13 12:35:38 -0500

    New fixed

--- js/changeLink.js
+++ js/changeLink.js
@@ -1,1 +1,10 @@
-$('body').hide().fadeIn(2500);
+// call back function example
+// $('body').hide().fadeIn(4000, function(){alert("done");});
+/* how jquery event works */
+    $('h1').click(function(){
+        $('this').text("Hide the image")
+        $('img').hide();
+        $('body').children('p').slideToggle();
+
+    });
+// console.log("I am here");
